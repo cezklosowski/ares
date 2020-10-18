2031c17d7244575b1169a7b460e7dbd28beb9128
cezklosowski <cez.klosowski@gmail.com>
2020-10-18 11:47:13 +0200

    add book_tags and relation OneToMany

--- src/main/java/com/cezklosowski/module/books/BooksEntity.java
+++ src/main/java/com/cezklosowski/module/books/BooksEntity.java
@@ -2,6 +2,7 @@ package com.cezklosowski.module.books;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.Set;
 
 @Entity
 @Table(name = "books")
@@ -20,6 +21,9 @@ public class BooksEntity {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "details_id", referencedColumnName = "id")
 	private BooksDetailsEntity details;
+
+	@OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+	private Set<BooksTagsEntity> tags;
 
 	public Long getId() {
 		return id;
@@ -54,6 +58,15 @@ public class BooksEntity {
 
 	public BooksEntity setDetails(BooksDetailsEntity details) {
 		this.details = details;
+		return this;
+	}
+
+	public Set<BooksTagsEntity> getTags() {
+		return tags;
+	}
+
+	public BooksEntity setTags(Set<BooksTagsEntity> tags) {
+		this.tags = tags;
 		return this;
 	}
 }
